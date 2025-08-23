6496216f2a243cd39986cd9bb978eeda81c10efd
tenel4 <mertenel@gmail.com>
2025-08-23 17:16:23 +0300

    initial setup

---
+++ services/settlement-service/src/main/java/com/finledger/settlement_service/SettlementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finledger.settlement_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementServiceApplication.class, args);
+	}
+
+}
